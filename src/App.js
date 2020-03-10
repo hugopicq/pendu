4e0c1c3cc753b13c5a6e71889b12e7b93261a0d3
hugopicq <picq.hugo@gmail.com>
2020-03-10 13:41:10 +0100

    Handling v1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+class App extends React.Component {
+
+  //static defaultProps = {}
+  //static propTypes = {}
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      testedLetters = [],
+      guessWord = '',
+      guessCount = 0
+    }
+  }
+
+  generateMask() {
+    const { guessWord, testedLetters } = this.state
+    mask = ''
+    foreach(letter in guessWord)
+      letter in testedLetters ? mask += letter : letter += '_'
+
+    return mask
+  }
+
+  handleLetterClick = letter => {
+    const { testedLetters, guessCount } = this.state
+    if(letter in testedLetters){
+      return
+    }
+    newGuessCount = guessCount + 1
+
+    this.setState({testedLetters: [...letter]})
+    this.setState({guessCount: newGuessCount})
+  }
+
+  render() {
+    /* TODO : masque de mot + tentatives + <div className="letters" /> */
+    const { guessWord, testedLetters } = this.state
+    const mask = this.generateMask()
+    const won = mask === guessWord
+
+    return (
+      <div className="pendu">
+        <p className="mask">{ mask }</p>
+        <div className="clavier">
+          {LETTERS.map((letter, index) => (
+            <div class="letter" key={index} onClick={this.handleLetterClick(letter)}>{letter}</div>
+          ))}
+        </div>
+      </div>
+    )
+  }
+
 }
 
 export default App;
